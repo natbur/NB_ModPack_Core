fe04491d7eda049cd3dd987869021ac0f6311fd1
Nathan Burke <n.burke@natbur.com>
2016-06-03 07:55:16 +0100

    Initial Commit

---
+++ Source/PostBuild.bat
@@ -0,0 +1,22 @@
+REM Copy this file to "PostBuild.bat"
+REM This is a local file copy after build.  Get it once and your .gitnore
+REM should handle it after that.  Make all your local copies at the end.
+
+REM Set this to your local RimWorld install path and CCL Assemblies directory
+Set InstalledCCLAssemblies="C:\Users\Nathan Burke\Downloads\Rimworld\Modpack\Mods\NB_ModPack_Core\Assemblies"
+
+if NOT EXIST %InstalledCCLAssemblies% (
+    echo Missing or invalid copy target:
+    echo %InstalledCCLAssemblies%
+    EXIT -1
+)
+
+echo Build Config: %1
+echo Build Target: %2
+echo Solution Path: %3
+echo CCL Install Path: %InstalledCCLAssemblies%
+
+echo Copy to RimWorld
+copy %2 %InstalledCCLAssemblies%
+
+:Finished
---
+++ Source/TerrainUpdateDef.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace NB_ModPack_Core
+{
+    public class TerrainUpdateDef : BuildableUpdateDef
+    {
+        #region XML Data
+        public List<TerrainDef> thingDefs;
+
+        public string holdSnow;
+
+        #endregion
+
+        [Unsaved]
+
+        #region Instance Data
+        protected bool holdSnowBool;
+
+        protected override List<T> GetDefs<T>()
+        {
+            return thingDefs.Cast<T>().ToList();
+        }
+
+        protected override string GetErrors()
+        {
+            var errors = base.GetErrors();
+
+            if (!holdSnow.NullOrEmpty() && !bool.TryParse(holdSnow, out holdSnowBool))
+            {
+                errors += "\n\tholdSnow must be a bool";
+            }
+
+            return errors;
+        }
+
+        #endregion
+
+        #region Process State
+
+        public override bool UpdateThings()
+        {
+            if (!base.UpdateThings())
+            {
+                return false;
+            }
+
+            var setHoldSnow = !holdSnow.NullOrEmpty();
+
+            foreach (var targetDef in thingDefs)
+            {
+                if (setHoldSnow)
+                {
+                    targetDef.holdSnow = holdSnowBool;
+                }
+            }
+
+            return true;
+        }
+
+        #endregion
+    }
+
+}
